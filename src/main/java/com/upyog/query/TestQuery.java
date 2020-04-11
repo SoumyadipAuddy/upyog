080e0438165b7eda6f171863fa484f0237f22458
Soumodeep Dutta <soumodeepd@soumodeepd@nrifintech.com>
2020-04-11 22:02:25 +0530

    Chnaged Controller class

--- src/main/java/com/upyog/query/TestQuery.java
+++ src/main/java/com/upyog/query/TestQuery.java
@@ -5,19 +5,21 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import org.apache.commons.beanutils.RowSetDynaClass;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
-
+import org.springframework.stereotype.Component;
+@Component
 public class TestQuery {
 	@Autowired
 	ApplicationContext context;
 
-	public ResultSet runQuery() {
+	public RowSetDynaClass runQuery() {
 		try {
 			Connection conn=context.getBean("connection", Connection.class);
 			PreparedStatement prepareStatement =conn.prepareStatement("select * from dbo.Product");
 			prepareStatement.execute();
-			return prepareStatement.getResultSet();
+			return new RowSetDynaClass(prepareStatement.getResultSet());
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
