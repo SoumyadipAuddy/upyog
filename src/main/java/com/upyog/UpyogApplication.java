49b4c60032d6d78fe969ec95b3cbaa33b0963e75
Soumodeep Dutta <soumodeepd@soumodeepd@nrifintech.com>
2020-04-11 22:41:03 +0530

    AUddy check:deployed code not

--- src/main/java/com/upyog/UpyogApplication.java
+++ src/main/java/com/upyog/UpyogApplication.java
@@ -1,33 +1,13 @@
 package com.upyog;
 
-import java.util.List;
-
-import org.apache.commons.beanutils.DynaBean;
-import org.apache.commons.beanutils.RowSetDynaClass;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.upyog.query.TestQuery;
-
 @SpringBootApplication
-public class UpyogApplication implements ApplicationRunner {
-	@Autowired
-	TestQuery ob;
+public class UpyogApplication {
 
 	public static void main(String[] args) {
-		SpringApplication upyogApplication = new SpringApplication(UpyogApplication.class);
-		upyogApplication.run(args);
-	}
-
-	@Override
-	public void run(ApplicationArguments args) throws Exception {
-		RowSetDynaClass dynaBeanList = ob.runQuery();
-		for (DynaBean row : (List<DynaBean>)dynaBeanList.getRows()) {
-			System.out.println(row.get("pname"));
-		}
+		SpringApplication.run(UpyogApplication.class, args);
 	}
 
 }
--- src/main/java/com/upyog/query/TestQuery.java
+++ src/main/java/com/upyog/query/TestQuery.java
@@ -2,21 +2,18 @@ package com.upyog.query;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import org.apache.commons.beanutils.RowSetDynaClass;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 @Component
 public class TestQuery {
 	@Autowired
-	ApplicationContext context;
+	Connection conn;
 
 	public RowSetDynaClass runQuery() {
 		try {
-			Connection conn=context.getBean("connection", Connection.class);
 			PreparedStatement prepareStatement =conn.prepareStatement("select * from dbo.Product");
 			prepareStatement.execute();
 			return new RowSetDynaClass(prepareStatement.getResultSet());
