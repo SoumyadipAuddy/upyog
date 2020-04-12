273f5d2052673efa07163ce620c9a587454bd59f
Soumodeep Dutta <soumodeepd@soumodeepd@nrifintech.com>
2020-04-13 03:10:44 +0530

    Modified Controller

--- src/main/java/com/upyog/controller/Controller.java
+++ src/main/java/com/upyog/controller/Controller.java
@@ -1,14 +1,44 @@
 package com.upyog.controller;
 
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.upyog.insert.QueryExecute;
+import com.upyog.insert.SellerReg;
 
 @RestController
 public class Controller {
-
+	@Autowired
+	ApplicationContext context;
 	@GetMapping(path = "/test")
 	public String test() {
-		return "Hello world from Spring boot!!!";
+		return "Hello home from Spring boot!!!#SocialDistancing";
+	}
+
+	@GetMapping(path = "/error")
+	public String error() {
+		return "Some Error occured!!I don't know what!";
+	}
+
+	@GetMapping(path = "/query")
+	public String query(@RequestParam(required = false) String id) {
+		QueryExecute ob=context.getBean("testQuery",QueryExecute.class);
+		return ob.runQuery("");
+	}
+
+	@PostMapping(path = "/registration_seller", consumes = {"application/json","text/plain"}, produces = {"application/json","text/plain"})
+	public @ResponseBody String registrationSeller(@RequestBody String jsonStr) {
+		return context.getBean("sellerReg",QueryExecute.class).runQuery(jsonStr);
+	}
+
+	@GetMapping(path = "/")
+	public String home() {
+		return "Hello Home!#SocialDistancing";
 	}
 }
