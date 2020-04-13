cc56a775700cd5310a6da2d89a930299863ce57e
Soumodeep Dutta <soumodeepd@soumodeepd@nrifintech.com>
2020-04-13 17:47:13 +0530

    Modification in Reg Seller

--- src/main/java/com/upyog/TestConnection.java
+++ src/main/java/com/upyog/TestConnection.java
@@ -10,30 +10,30 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.upyog.insert.SellerReg;
 //@SpringBootApplication
-public class TestConnection implements ApplicationRunner{
-	
-	@Autowired
-	Connection conn;
-	@Autowired
-	SellerReg ob;
-public static void main(String[] args) {
-	SpringApplication.run(TestConnection.class, args);
-}
-
-	@Override
-	public void run(ApplicationArguments args) throws Exception {
-		String json="{\r\n" + 
-				"	firstName: \"firstName\",\r\n" + 
-				"	lastName: \"lastName\",\r\n" + 
-				"	mobileNo: 9876543210,\r\n" + 
-				"	email: \"abc@gmail.com\",\r\n" + 
-				"	password:\"asdf@1234\",\r\n" + 
-				"	gstNumber:\"\",\r\n" + 
-				"	address: \"asd, vhjk, 567890\",\r\n" + 
-				"	coordinates: \"(12.3456, 12.6788)\", // maps coordinates\r\n" + 
-				"	customer_status: \"buyer\" \r\n" + 
-				"}";
-		System.out.println( ob.runQuery(json));
-	}
+public class TestConnection {
+//
+//	@Autowired
+//	Connection conn;
+//	@Autowired
+//	SellerReg ob;
+//public static void main(String[] args) {
+//	SpringApplication.run(TestConnection.class, args);
+//}
+//
+//	@Override
+//	public void run(ApplicationArguments args) throws Exception {
+//		String json="{\r\n" + 
+//				"	firstName: \"firstName\",\r\n" + 
+//				"	lastName: \"lastName\",\r\n" + 
+//				"	mobileNo: 9876543210,\r\n" + 
+//				"	email: \"abc@gmail.com\",\r\n" + 
+//				"	password:\"asdf@1234\",\r\n" + 
+//				"	gstNumber:\"\",\r\n" + 
+//				"	address: \"asd, vhjk, 567890\",\r\n" + 
+//				"	coordinates: \"(12.3456, 12.6788)\", // maps coordinates\r\n" + 
+//				"	customer_status: \"buyer\" \r\n" + 
+//				"}";
+//		System.out.println( ob.runQuery(json));
+//	}
 	
 }
--- src/main/java/com/upyog/UpyogApplication.java
+++ src/main/java/com/upyog/UpyogApplication.java
@@ -9,5 +9,4 @@ public class UpyogApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UpyogApplication.class, args);
 	}
-
 }
