b3ecb0c2b36a5e5bf6d1ebe3b46aa9d505f28c67
SoumyadipAuddy <soumyadip.auddy@gmail.com>
2020-04-13 12:14:17 +0530

    ready for heroku

--- src/test/java/com/upyog/UpyogApplicationTests.java
+++ src/test/java/com/upyog/UpyogApplicationTests.java
@@ -3,10 +3,10 @@ package com.upyog;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class UpyogApplicationTests {
 
-	@Test
+	
 	void contextLoads() {
 	}
 
